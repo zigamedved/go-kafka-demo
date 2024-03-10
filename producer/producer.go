67471ec5b308a835e629930f74991427051974ce
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2024-03-10 16:08:32 +0100

    add producer code

---
+++ producer/producer.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/IBM/sarama"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Comment struct {
+	Text string `form:"text" json:"text"`
+}
+
+func main() {
+	app := fiber.New()
+	app.Post("/comments", createComment)
+	app.Listen(":3000")
+}
+
+func ConnectProducer(brokersUrl []string) (sarama.SyncProducer, error) {
+	cfg := sarama.NewConfig()
+	cfg.Producer.Return.Successes = true
+	cfg.Producer.RequiredAcks = sarama.WaitForAll
+	cfg.Producer.Retry.Max = 5
+
+	conn, err := sarama.NewSyncProducer(brokersUrl, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func PushCommentToQueue(topic string, message []byte) error {
+	brokersUrl := []string{"localhost:29092"}
+	producer, err := ConnectProducer(brokersUrl)
+	if err != nil {
+		return err
+	}
+	defer producer.Close()
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Message is stored in topic: %s, partition: %d, offset: %d", topic, partition, offset)
+
+	return nil
+}
+
+func createComment(c *fiber.Ctx) error {
+	cmt := new(Comment)
+	if err := c.BodyParser(cmt); err != nil {
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+		return err
+	}
+
+	cmtInBytes, err := json.Marshal(cmt)
+	if err != nil {
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+		return err
+	}
+
+	err = PushCommentToQueue("comments", cmtInBytes)
+	if err != nil {
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+		return err
+	}
+
+	return c.JSON(&fiber.Map{
+		"success": true,
+		"message": "Comment pushed to queue successfully",
+		"comment": cmt,
+	})
+}
